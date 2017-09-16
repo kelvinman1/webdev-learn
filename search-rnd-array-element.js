7d8363bc0aa078615883be18f5b682b83ecfd9eb
serg <turetskiserg@gmail.com>
2017-09-16 08:30:49 +0300

    Сделал проверку на положительное целое.

--- search-rnd-array-element.js
+++ search-rnd-array-element.js
@@ -12,18 +12,25 @@ for (i = 0; i < n; i++){
 console.log('Filled random array: ');
 console.log(out);
 
-out = '';
-var isFound = false;
-let k = prompt('Введите искомое число:', 0);
-for (i = 0; i < n; i++){
-	if (a[i] == k){
-		isFound = true;
-		out = 'Element ' + k + ' is found!';
-		break;
+(function (){
+	out = '';
+	var isFound = false;
+	let k = prompt('Введите искомое число:', 0);
+	if ((k == 0 || k ^ 0 == k) && k >= 0) ;
+	else{
+		alert('Вводимое число не является положительным целым числом!');
+		return;
 	}
-}
-if (!isFound){
-	out = 'Element ' + k + ' not found!';
-}
-console.log(out);
-
+	
+	for (i = 0; i < n; i++){
+		if (a[i] == k){
+			isFound = true;
+			out = 'Element ' + k + ' is found!';
+			break;
+		}
+	}
+	if (!isFound){
+		out = 'Element ' + k + ' not found!';
+	}
+	console.log(out);
+})();
