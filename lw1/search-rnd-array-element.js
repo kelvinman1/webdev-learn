be72faa8983eb9862af7970dcd4e2842de1f0a96
serg <turetskiserg@gmail.com>
2017-09-16 14:06:17 +0300

    Заменил табуляцию на пробелы.

--- lw1/search-rnd-array-element.js
+++ lw1/search-rnd-array-element.js
@@ -1,45 +1,45 @@
-function getRandom (max) {
+﻿function getRandom (max) {
   return parseInt(Math.random() * max);
 }
 
 const maxSizeArray = 10;
 
 function findValue (array, i, value) {
-	if (array[i] == value) {
-		return true;
-	}
-	
-	if (i + 1 >= maxSizeArray) {
-		return false;
-	}
-	
-	return findValue(array, i + 1, value);
+  if (array[i] == value) {
+    return true;
+  }
+  
+  if (i + 1 >= maxSizeArray) {
+    return false;
+  }
+  
+  return findValue(array, i + 1, value);
 }
 
 var randomArray = [];
 var output = '';
 for (i = 0; i < maxSizeArray; i++) {
-	randomArray[i] = getRandom(15);
-	output += randomArray[i] + ' ';
+  randomArray[i] = getRandom(15);
+  output += randomArray[i] + ' ';
 }
 console.log('Filled random array: ');
 console.log(output);
 
 (function () {
-	output = '';
-	const searchValue = parseInt(prompt('Введите искомое число:', 0));
-	
-	if (isNaN(searchValue) || searchValue < 0) {
-		alert('Вводимое число не является положительным целым числом!');
-		return;
-	}
-	
-	if (findValue(randomArray, 0, parseInt(searchValue))) {
-		output = 'Element ' + searchValue + ' is found!';
-	}
-	else {
-		output = 'Element ' + searchValue + ' not found!';
-	}
-	
-	console.log(output);
+  output = '';
+  const searchValue = parseInt(prompt('Введите искомое число:', 0));
+  
+  if (isNaN(searchValue) || searchValue < 0) {
+    alert('Вводимое число не является положительным целым числом!');
+    return;
+  }
+  
+  if (findValue(randomArray, 0, parseInt(searchValue))) {
+    output = 'Element ' + searchValue + ' is found!';
+  }
+  else {
+    output = 'Element ' + searchValue + ' not found!';
+  }
+  
+  console.log(output);
 })();
