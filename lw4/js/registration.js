039863c1ba0b7eef3ac48c87df734459aa5db269
serg <turetskiserg@gmail.com>
2017-10-14 14:19:37 +0300

    Скопировал lw3.
    Скопировал lw3.

---
+++ lw4/js/registration.js
@@ -0,0 +1,62 @@
+function validEmail(email){
+  var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+  return pattern.test(email);
+}
+
+class RegistrationForm {
+  constructor(formElement) {
+    this.form = formElement;
+    this.formEmail = $(formElement).find('.form_email').val();
+    this.formPassword = $(formElement).find('.form_password').val();
+    this.formPasswordRepeat = $(formElement).find('.form_password_repeat').val();
+    this.formCheckbox = $(formElement).find('.form_checkbox').prop('checked');
+  }
+  validate() {
+    if (!validEmail(this.formEmail)){
+      this.errorMsg = 'Не корректный email!';
+      return false;
+    }
+    
+    if (this.formPassword.length < 6){
+      this.errorMsg = 'Пароль должен быть длиннее 6 символов!';
+      return false;
+    }
+    
+    if (this.formPassword !== this.formPasswordRepeat){
+      this.errorMsg = 'Пароль не совпадает!';
+      return false;
+    }
+    
+    if (!this.formCheckbox){
+      this.errorMsg = 'Вы не согласны с правилами сайта!';
+      return false;
+    }
+    
+    return true;
+  }
+  success() {
+    alert('Вы зарегистрированы.');
+  }
+  error() {
+    alert(this.errorMsg);
+  }
+}
+
+$('.registration_form').on('submit', function () {
+  var form = new RegistrationForm(document.getElementsByClassName('registration_form')[0]);
+  if (form.validate()) {
+    form.success();
+  }
+  else {
+    form.error();
+  }
+  return false;
+});
+
+
+
+
+
+
+
+
