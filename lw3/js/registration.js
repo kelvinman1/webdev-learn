d4d858acdc7f5fc2c1b818a5e604e7eaf7752b3d
serg <turetskiserg@gmail.com>
2017-10-14 13:40:20 +0300

    Исправил написание имен. Добавил скобки.
    Исправил написание имен. Добавил скобки.

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -4,31 +4,31 @@ function validEmail(email){
 }
 
 class RegistrationForm {
-  constructor(form_element) {
-    this.form = form_element;
-    this.form_email = form_element.getElementsByClassName('form_email')[0].value;
-    this.form_password = form_element.getElementsByClassName('form_password')[0].value;
-    this.form_password_repeat = form_element.getElementsByClassName('form_password_repeat')[0].value;
-    this.form_checkbox = form_element.getElementsByClassName('form_checkbox')[0].checked;
+  constructor(formElement) {
+    this.form = formElement;
+    this.formEmail = formElement.getElementsByClassName('form_email')[0].value;
+    this.formPassword = formElement.getElementsByClassName('form_password')[0].value;
+    this.formPasswordRepeat = formElement.getElementsByClassName('form_password_repeat')[0].value;
+    this.formCheckbox = formElement.getElementsByClassName('form_checkbox')[0].checked;
   }
   validate() {
-    if (!validEmail(this.form_email)){
-      this.error_msg = 'Не корректный email!';
+    if (!validEmail(this.formEmail)){
+      this.errorMsg = 'Не корректный email!';
       return false;
     }
     
-    if (this.form_password.length < 6){
-      this.error_msg = 'Пароль должен быть длиннее 6 символов!';
+    if (this.formPassword.length < 6){
+      this.errorMsg = 'Пароль должен быть длиннее 6 символов!';
       return false;
     }
     
-    if (this.form_password !== this.form_password_repeat){
-      this.error_msg = 'Пароль не совпадает!';
+    if (this.formPassword !== this.formPasswordRepeat){
+      this.errorMsg = 'Пароль не совпадает!';
       return false;
     }
     
-    if (!this.form_checkbox){
-      this.error_msg = 'Вы не согласны с правилами сайта!';
+    if (!this.formCheckbox){
+      this.errorMsg = 'Вы не согласны с правилами сайта!';
       return false;
     }
     
@@ -38,17 +38,18 @@ class RegistrationForm {
     alert('Вы зарегистрированы.');
   }
   error() {
-    alert(this.error_msg);
+    alert(this.errorMsg);
   }
 }
 
-var submit_registration = document.getElementById('submit_registration');
-submit_registration.onclick = function () {
+document.getElementById('submit_registration').onclick = function () {
   var form = new RegistrationForm(document.getElementsByClassName('auth_form')[0]);
-  if (form.validate())
+  if (form.validate()) {
     form.success();
-  else
+  }
+  else {
     form.error();
+  }
   return false;
 };
 
